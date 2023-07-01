1034c357463396a90fce6f1204dd84642d6343db
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-07-01 20:30:26 +0300

    Adding Laravel Movies App using Laravel 7

---
+++ Projects/Laravel_movies_app (Laravel 7)/laravel7-movies-app/resources/views/layouts/main.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CENTRAL PERK</title>
+
+    <link rel="stylesheet" href="/css/main.css">
+    <livewire:styles>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+</head>
+<body class="font-sans bg-gray-900 text-white">
+    <nav class="border-b border-gray-800">
+        <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between px-4 py-6">
+            <ul class="flex flex-col md:flex-row items-center">
+                <li>
+                    <a href="{{ route('movies.index') }}">
+                        <text xmlns="http://www.w3.org/2000/svg" style="font-weight: bold;
+    font-size: 21px;" transform="matrix(1.97767 0 0 2.32832 -726.442 -601.282)" stroke="#000" font-style="normal" font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Special Elite'" font-size="24" id="svg_1" y="284.21291" x="374.53318" stroke-width="0" fill="#000000">CENTRAL PERK</text>
+                    </a>
+                </li>
+                <li class="md:ml-16 mt-3 md:mt-0">
+                    <a href="{{ route('movies.index') }}" class="hover:text-gray-300">Movies</a>
+                </li>
+                <li class="md:ml-6 mt-3 md:mt-0">
+                    <a href="{{ route('tv.index') }}" class="hover:text-gray-300">TV Shows</a>
+                </li>
+                <li class="md:ml-6 mt-3 md:mt-0">
+                    <a href="{{ route('actors.index') }}" class="hover:text-gray-300">Actors</a>
+                </li>
+            </ul>
+            <div class="flex flex-col md:flex-row items-center">
+                <livewire:search-dropdown>
+                <div class="md:ml-4 mt-3 md:mt-0">
+                    <a href="#">
+                        <img src="/img/avatar.jpg" alt="avatar" class="rounded-full w-8 h-8">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
+    <footer class="border border-t border-gray-800">
+        <div class="container mx-auto text-sm px-4 py-6">
+            Powered by <a href="https://www.themoviedb.org/documentation/api" class="underline hover:text-gray-300">TMDb API</a>
+            <br>
+            <span>By Ahmed M. Fathy </span>
+        </div>
+
+
+
+    </footer>
+    <livewire:scripts>
+    @yield('scripts')
+</body>
+</html>
